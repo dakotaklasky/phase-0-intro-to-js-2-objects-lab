984e0331f19d67ab09623888222817f43a936a82
Dakota <dakota.klasky@gmail.com>
2024-07-03 15:03:09 -0400

    created functions

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+
+const employee = {
+    name : "Dakota",
+    streetAddress: "4404 Pebble Pond Dr",
+};
+
+function updateEmployeeWithKeyAndValue(employee,key,value){
+    const employeeCopy = {...employee};
+    employeeCopy[key] = value;
+    return employeeCopy;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value){
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee,key){
+    const employeeCopy = {...employee};
+    delete employeeCopy[key];
+    return employeeCopy;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee,key){
+    delete employee[key];
+    return employee;
+}
